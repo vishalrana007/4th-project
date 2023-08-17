87281653fb7438aa9c8ca45d97132833854ab778
Vishal Rana <134679649+vishalrana007@users.noreply.github.com>
2023-08-17 12:16:36 +0530

    commit1

---
+++ script.js
@@ -0,0 +1,79 @@
+function wecare(){
+    location.assign("https://www.sotc.in/reach-us");
+}
+function store(){
+    location.assign("https://stores.sotc.in/");
+}
+function flight(){
+    location.assign("https://www.sotc.in/flights");
+}
+function hotel(){
+    location.assign("https://www.sotc.in/hotels");
+}
+function cruise(){
+    location.assign("https://cruise.sotc.in/");
+}
+function vistara(){
+    location.assign("https://getaways.airvistara.com/?src=vistara_getaways_banner_1")
+}
+function anda(){
+    location.assign('https://www.sotc.in/india-tour-packages/andaman-and-nicobar-islands-tour-packages?src=homepage_tid_1')
+}
+function kerela(){
+    location.assign('https://www.sotc.in/india-tour-packages/kerala-tour-packages?src=homepage_tid_2')
+}
+function guj(){
+    location.assign('https://www.sotc.in/india-tour-packages/gujarat-tour-packages?src=homepage_tid_3')
+}
+function hp(){
+    location.assign('https://www.sotc.in/india-tour-packages/himachal-pradesh-tour-packages?src=homepage_tid_4')
+}
+function kasmir(){
+    location.assign('https://www.sotc.in/india-tour-packages/jammu-and-kashmir-tour-packages?src=homepage_tid_5')
+}
+function dubai(){
+    location.assign('https://www.sotc.in/international-tour-packages/dubai-tour-packages?src=homepage_tid_2')
+}
+function thailand(){
+    location.assign('https://www.sotc.in/international-tour-packages/thailand-tour-packages?src=homepage_tid_5')
+}
+function vietnam(){
+    location.assign('https://www.sotc.in/international-tour-packages/vietnam-tour-packages?src=homepage_tid_1')
+}
+function south(){
+    location.assign('https://www.sotc.in/international-tour-packages/south-africa-tour-packages?src=homepage_tid_4')
+}
+function japan(){
+    location.assign('https://www.sotc.in/international-tour-packages/japan-tour-packages?src=homepage_tid_3')
+}
+function xyz(){
+ let  x=document.getElementById('email').value;
+    alert("succesfully added"+" "+x)}
+    ;
+     function honey(){
+        location.assign('https://www.sotc.in/blog/destinations/india/best-honeymoon-destinations-in-india-in-may')
+     }   ; 
+     function pharrow(){
+        location.assign('https://www.sotc.in/blog/holiday-themes/valley-of-the-kings-where-pharaohs-meet-their-gods')
+     };
+     function andaman(){
+        location.assign('https://www.sotc.in/blog/destinations/india/plan-budget-trip-andaman')
+     };
+
+function fb(){
+    location.assign('https://www.facebook.com/SOTCHolidays/')
+}
+function utube(){
+    location.assign('https://www.youtube.com/user/KuoniTravelIndia')
+}
+function linkedin(){
+    location.assign('https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fsotc%2Fabout%2F')
+}
+function twiter(){
+    location.assign('https://twitter.com/SOTCTravel')
+}
+function insta(){
+    location.assign('https://www.instagram.com/sotc_holidays/')
+}
+
+     
